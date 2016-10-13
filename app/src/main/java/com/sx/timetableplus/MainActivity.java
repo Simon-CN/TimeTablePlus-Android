2e73d74ca91483397100f90a79bde17d3b1e5aca
SX <txzb01@live.com>
2016-10-13 13:07:34 +0800

    first commit

--- app/src/main/java/com/sx/timetableplus/MainActivity.java
+++ app/src/main/java/com/sx/timetableplus/MainActivity.java
@@ -1,13 +1,33 @@
 package com.sx.timetableplus;
 
+import android.databinding.DataBindingUtil;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.sx.timetableplus.View.Fragment.HomeContentFragment;
+import com.sx.timetableplus.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(MainActivity.this, R.layout.activity_main);
+        initToolbar();
+
+        getSupportActionBar().hide();
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        HomeContentFragment content = new HomeContentFragment();
+        ft.replace(R.id.content_fram, content);
+        ft.commit();
+    }
+
+    protected void initToolbar() {
+        mBinding.toolbarMain.setTitle("课程表");
     }
 }
