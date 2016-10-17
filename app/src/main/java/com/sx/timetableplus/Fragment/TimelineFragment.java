0685609fc38577572dfe7808565b1d8b30866de6
SX <txzb01@live.com>
2016-10-17 16:44:32 +0800

    layout

---
+++ app/src/main/java/com/sx/timetableplus/Fragment/TimelineFragment.java
@@ -0,0 +1,28 @@
+package com.sx.timetableplus.Fragment;
+
+import android.databinding.DataBindingUtil;
+import android.databinding.tool.DataBindingBuilder;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sx.timetableplus.R;
+import com.sx.timetableplus.databinding.FragmentTimelineBinding;
+
+/**
+ * Created by sx on 2016/10/17.
+ */
+
+public class TimelineFragment extends Fragment {
+    FragmentTimelineBinding mBinding;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_timeline, container, false);
+        return mBinding.getRoot();
+    }
+}
--- app/src/main/java/com/sx/timetableplus/MainActivity.java
+++ app/src/main/java/com/sx/timetableplus/MainActivity.java
@@ -1,44 +1,117 @@
 package com.sx.timetableplus;
 
 import android.databinding.DataBindingUtil;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.Window;
+import android.support.v7.widget.PopupMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.RadioGroup;
 
-import com.sx.timetableplus.View.Fragment.HomeContentFragment;
+import com.sx.timetableplus.Adapter.MainPagerAdapter;
+import com.sx.timetableplus.Fragment.SocialFragment;
+import com.sx.timetableplus.Fragment.TimelineFragment;
+import com.sx.timetableplus.Fragment.TimetableFragment;
 import com.sx.timetableplus.databinding.ActivityMainBinding;
 
-import org.antlr.v4.Tool;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    ActivityMainBinding mBinding;
-    Toolbar toolbar;
+    private ActivityMainBinding mBinding;
+    private PopupMenu pMenu;
+    private Menu moreMenu;
+    private MainPagerAdapter mAdapter;
+    private List<Fragment> fragmentList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(MainActivity.this, R.layout.activity_main);
-        initToolbar();
-        toolbar = mBinding.toolbarMain;
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mBinding.mainDrawerLy, toolbar, R.string.navigation_open, R.string.navigation_close);
-        mBinding.mainDrawerLy.setDrawerListener(toggle);
-        toggle.syncState();
+        initView();
+        initListener();
 
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        HomeContentFragment content = new HomeContentFragment();
-        ft.replace(R.id.content_fram, content);
-        ft.commit();
     }
 
-    protected void initToolbar() {
-        toolbar = mBinding.toolbarMain;
-        toolbar.setTitle("课程表");
-        setSupportActionBar(toolbar);
+    private void initView() {
+        pMenu = new PopupMenu(this, mBinding.moreButton);
+        moreMenu = pMenu.getMenu();
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_timetable_setting, moreMenu);
 
+        initViewPager();
+        mBinding.homeRadioGroup.check(R.id.home_radio_button);
+    }
+
+    private void initListener() {
+        mBinding.hamburgerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mBinding.mainDrawerLy.isDrawerOpen(GravityCompat.START)) {
+                    mBinding.mainDrawerLy.closeDrawer(GravityCompat.START);
+                } else {
+                    mBinding.mainDrawerLy.openDrawer(GravityCompat.START);
+                }
+
+            }
+        });
+
+        mBinding.mainViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                selectPage(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        mBinding.homeRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i) {
+                    case R.id.home_radio_button:
+                        selectPage(0);
+                        break;
+                    case R.id.timeline_radio_button:
+                        selectPage(1);
+                        break;
+                    case R.id.social_radio_button:
+                        selectPage(2);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+    }
+
+    private void initViewPager() {
+        fragmentList.add(new TimetableFragment());
+        fragmentList.add(new TimelineFragment());
+        fragmentList.add(new SocialFragment());
+
+        mAdapter = new MainPagerAdapter(getSupportFragmentManager(), fragmentList);
+        mBinding.mainViewPager.setAdapter(mAdapter);
+    }
+
+    private void selectPage(int position) {
+        mBinding.homeRadioGroup.check(mBinding.homeRadioGroup.getChildAt(position).getId());
+        mBinding.mainViewPager.setCurrentItem(position);
+    }
+
+    public void popupmenu(View view) {
+        pMenu.show();
     }
 }
