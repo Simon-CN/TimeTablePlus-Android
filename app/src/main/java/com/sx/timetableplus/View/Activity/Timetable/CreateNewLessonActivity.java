1f91281e525bc2ac4def062b3c73e96b0c9d5933
SX <txzb01@live.com>
2016-12-12 17:33:12 +0800

    create lesson

--- app/src/main/java/com/sx/timetableplus/View/Activity/Timetable/CreateNewLessonActivity.java
+++ app/src/main/java/com/sx/timetableplus/View/Activity/Timetable/CreateNewLessonActivity.java
@@ -1,9 +1,12 @@
 package com.sx.timetableplus.View.Activity.Timetable;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatDelegate;
+import android.view.View;
 
 import com.sx.timetableplus.View.Activity.BaseActivity;
+import com.sx.timetableplus.View.Custom.TimePickDialog;
 import com.sx.timetableplus.databinding.ActivityCreateNewLessonBinding;
 import com.sx.timetableplus.R;
 
@@ -14,10 +17,6 @@ import com.sx.timetableplus.R;
 public class CreateNewLessonActivity extends BaseActivity {
     ActivityCreateNewLessonBinding mBinding;
 
-    static {
-        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
-    }
-
     @Override
     protected void getLayoutResource() {
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_create_new_lesson);
@@ -26,6 +25,13 @@ public class CreateNewLessonActivity extends BaseActivity {
     @Override
     protected void initView() {
         initToolbar();
+        mBinding.selectLessonTimeTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickDialog mDialog = new TimePickDialog(CreateNewLessonActivity.this);
+                mDialog.show();
+            }
+        });
     }
 
     protected void initToolbar() {
@@ -33,4 +39,5 @@ public class CreateNewLessonActivity extends BaseActivity {
         toolbar.setTitle(R.string.create_lesson);
         super.initToolbar();
     }
+
 }
